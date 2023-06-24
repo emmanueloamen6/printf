517442286866e8852e68ede974f3bd966e7b48ed
emmanueloamen6 <emmanueloamen6@gmail.com>
2023-06-24 09:15:10 -0700

    _printf.c

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+void print_buffer(char buffer[], int *buff_ind)
+int _printf(const char *format, ...);
+#endif
+
